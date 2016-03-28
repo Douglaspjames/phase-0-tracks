8ad17252f7d17a63c18be5388a696c398d5513c6
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-03-27 20:51:58 -0500

    added changes to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,18 +12,30 @@ while surveys <= number_of_employees
   garlic_bread = gets.chomp
   puts "Would you like to enroll in the company's health insurance?(Y/N)"
   health_insurance = gets.chomp
-
-  if age == (2016 - year) && (garlic_bread == "Y" && health_insurance == "Y") && (name == "Drake Cula" || name == "Tu Fang")
-      puts "Definitely a vampire"
-  elsif age != (2016 - year) && (garlic_bread == "N" && health_insurance == "N")
-  	puts "Almost certainly a vampire!"
-  elsif age != (2016 - year) && (garlic_bread == "N" || health_insurance == "N")
-  	puts "Probably a vampire!"
-  elsif age == (2016 - year) && (name != "Drake Cula" || name != "Tu Fang") && (garlic_bread == "Y" || health_insurance == "Y")
-      puts "Probably not a vampire."
-  else
-      puts "Results inconclusive"
+  puts "Name any allergies one at a time. Type done when finished"
+  loop do
+  allergies = gets.chomp
+   if allergies == 'done'
+   break
+   end
+   if allergies == "sunshine"
+   puts "Probably a vampire"
+   break
+   end
   end
+
+    if age == (2016 - year) && (garlic_bread == "Y" && health_insurance == "Y") && (name == "Drake Cula" || name == "Tu Fang")
+        puts "Definitely a vampire"
+    elsif age != (2016 - year) && (garlic_bread == "N" && health_insurance == "N")
+    	puts "Almost certainly a vampire!"
+    elsif age != (2016 - year) && (garlic_bread == "N" || health_insurance == "N")
+    	puts "Probably a vampire!"
+    elsif age == (2016 - year) && (name != "Drake Cula" || name != "Tu Fang") && (garlic_bread == "Y" || health_insurance == "Y")
+        puts "Probably not a vampire."
+    else
+        puts "Results inconclusive"
+    end
+
   puts "Your name is #{name}"
   puts "You're #{age} years old"
     if garlic_bread == "Y"
@@ -36,7 +48,8 @@ while surveys <= number_of_employees
     else
     	puts "You don't want to enroll in health insurance"
     end
-  #puts "#{garlic_bread ? 'You would like garlic bread' : 'You wouldn\'t like garlic bread'}"
-  #puts "#{health_insurance ? 'You would like to enroll in health insurance' : 'You don\'t want to enroll in health insurance'}"
+
   surveys +=1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
