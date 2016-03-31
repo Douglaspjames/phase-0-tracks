ec9bda599eeda80887241801ad064ffcfb2a11c3
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-03-30 23:10:06 -0500

    worked with pair on iteration.rb

---
+++ ruby/iteration.rb
@@ -0,0 +1,26 @@
+def test_method 
+  puts "Hello, nice to meet you!"
+  yield("Doug", "Hank")
+end
+
+test_method { |name1, name2| puts "#{name1} and #{name2}" }
+
+
+
+hobbies= ['tennis', 'movies', 'coding', 'cars']
+cars ={color:"blue", brand:"subaru", seat:5}
+
+hobbies.each do |x|
+  puts "I like #{x}"
+end
+
+cars.each do |y, z|
+  puts "My car is  #{z}"
+end
+
+hobbies.map! do |hobby|
+  puts hobby
+  hobby.reverse
+end
+p hobbies
+
