14e1b1a9ad8f6c04f9460b6301a507a0246b7509
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-05 17:58:09 -0500

    modified puppy.rb

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,14 +1,14 @@
-SPECIES ------------------------
-Earus Flopadorableus
+#SPECIES ------------------------
+#Earus Flopadorableus
 
-CHARACTERISTICS ----------------
-Ear floppiness: varies
-Friendly: yes
-poop per day count: 2
-Name: varies
-Color: varies
+#CHARACTERISTICS ----------------
+#Ear floppiness: varies
+#Friendly: yes
+#poop per day count: 2
+#Name: varies
+#Color: varies
 
-BEHAVIOR -----------------------
-Cuddle
-Sniff
-poop
+#BEHAVIOR -----------------------
+#Cuddle
+#Sniff
+#poop
