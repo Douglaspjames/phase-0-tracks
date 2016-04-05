f65a07757362d312244ca59deb71134081e600c2
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-05 16:42:30 -0500

    added notes to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Earus Flopadorableus
+
+CHARACTERISTICS ----------------
+Ear floppiness: varies
+Friendly: yes
+poop per day count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Cuddle
+Sniff
+poop
