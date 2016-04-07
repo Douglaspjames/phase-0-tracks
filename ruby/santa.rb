e559298bd72007a498f0120170ac2aebeedbb2b1
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-06 21:11:13 -0500

    santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,16 +15,41 @@ class Santa
   def eat_milk_and_cookies(cookie)
   	puts "That was a good #{cookie} cookie!"
   end
+
+  def age
+  	@age
+  end
+
+  def array
+  	@reindeer_ranking
+  end
+
+  def age_change=
+  	@age = @age +1
+  end
+
 end
+
+#p reindeer_ranking.get_mad_at("rudolph")
 
 fred_claus = Santa.new
 fred_claus.speak
 fred_claus.eat_milk_and_cookies("oatmeal raisin")
 
+
+puts "Fred Claus is #{@age} year old!"
+
 santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+santas << Santa.new("male", "white")
+santas << Santa.new("female", "latino")
+santas << Santa.new("male", "russian")
+santas << Santa.new("male", "latvian")
+
+
+
+
+
+# santas.each do |people|
+#   people.speak("hello")
+#   people.eat_milk_and_cookies("chocalate chip")
 
