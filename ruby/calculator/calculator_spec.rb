23d20aac55d8f5f055b5278e99bd66baa242e79a
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-08 21:52:37 -0500

    worked on 6.6 with pair

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -7,7 +7,6 @@
 # 'describe' block for each group of tests  
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
-
 require_relative 'calculator'
 
 describe Calculator do
@@ -23,5 +22,9 @@ describe Calculator do
 
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
+  end
+  
+  it "divides two integers" do
+    expect(calculator.divide(8,4)).to eq 2
   end
 end
