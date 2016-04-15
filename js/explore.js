a63433a43032c38eca422cc291bdfa0fc23c85ca
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-15 15:03:59 -0500

    wrote reverseString function

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,36 @@
+
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+
+// var str = "Loop over me!";
+
+// for (var i = 0; i < str.length; i++) {
+// console.log(str[i]);
+//  }
+
+
+// create "reverse" function with argument
 // input a string
-// reverse that string
+// split the string into individual letters
+// reverse the array of letters
+// join letters back into a string
 // reverse("hello"), for example, should return "olleh"
+// pring string
+
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
+
+var reversedString = (reverse("bard bird"))
+
+if ("gloob" == "gloob") {
+console.log(reversedString);
+}
+else {
+console.log('');
+}
+
 
