d9cd1e9a7ed2c6acbdb4d98f1344cf673a351386
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-17 15:14:28 -0500

    calc

--- ruby/week_5/calculator.rb
+++ ruby/week_5/calculator.rb
@@ -17,27 +17,26 @@ end
 #calculate(4, '*', 5)
 #calculate(20, '/', 5)
 
-index = 0
+totals = {}
+
 loop do 
-puts "please enter your calculation"
-calc = gets.chomp
-totals = [calc]
-  if calc == "done"
+puts "please enter your calculation (Type 'done' to exit)"
+data = gets.chomp
+  if data == "done"
   	puts "thank you"
   	break
   else
   #calc = gets.chomp
-  calc = calc.split(' ')
+  calc = data.split(' ')
   calc[0] = calc[0].to_i
   calc[2] = calc[2].to_i
-  p calculate(calc[0], calc[1], calc[2])
-  totals = [calculate(calc[0], calc[1], calc[2])]
-  list_totals = totals + []
-  index +=1
+  answer = calculate(calc[0], calc[1], calc[2])
+  totals[data] = answer
+  puts answer
   end
   #list_totals = totals
 end
-puts "#{index} calculations performed"
+#puts "#{index} calculations performed"
 
 #p list_totals
 
