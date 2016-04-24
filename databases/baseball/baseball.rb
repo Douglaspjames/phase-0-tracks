ca07f898192420ff627a619300e430cd13b481e0
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-23 23:17:29 -0500

    added code to baseball.rb

--- databases/baseball/baseball.rb
+++ databases/baseball/baseball.rb
@@ -10,34 +10,98 @@ require 'faker'
 db = SQLite3::Database.new("baseball.db")
 db.results_as_hash = true
 
-# learn about fancy string delimiters
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
+#create lineup
+create_lineup_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS lineup(
     id INTEGER PRIMARY KEY,
+    position VARCHAR(255),
     name VARCHAR(255),
-    age INT
+    bench_id INT,
+    FOREIGN KEY (bench_id) REFERENCES bench_players(id)
   )
 SQL
 
-# create a kittens table (if it's not there already)
-db.execute(create_table_cmd)
+create_bench_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS bench_players(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
 
-# add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+# create lineup and pitchers tables (if it's not there already)
+db.execute(create_lineup_cmd)
+db.execute(create_pitchers_cmd)
 
-# add LOOOOTS of kittens!
-# so. many. kittens. 
-#KittenExplosion
-def create_kitten(db, name, age)
-  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+
+def create_bench_players(db, name)
+  db.execute("INSERT INTO bench_players (name) VALUES (?)", [name])
 end
 
-10000.times do
-  create_kitten(db, Faker::Name.name, 0)
+5.times do
+  create_bench_players(db, Faker::Name.name)
 end
 
-# explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+#============================================================================#
+
+#method to create player lineup
+
+def create_lineup(db, name)
+	db.execute("INSERT INTO players (position, name) VALUES (?, ?)", [position, name])
+	end
+
+def add_1B(db, id, 1B)
+	db.execute("UPDATE players SET 1B=? WHERE id=?", [1B, id])
+
+
+
+
+
+
+
+
+
+
+
+
+
+def print_lineup(db)
+	puts "Here is your lineup:"
+	lineup = db.execute("SELECT * FROM lineup")
+	lineup.each do |player|
+		puts "#{player['position']}. #{player['name']}"
+	end
+end
+
+
+#Print pitchers!!!!!
+
+
+    print "Enter the name of 1B: "
+    1B = gets.chomp
+    
+    print "Enter the name of 2B: "
+    2B = gets.chomp
+    
+    print "Enter the name of SS: "
+    SS = gets.chomp
+
+    print "Enter the name of 3B: "
+    3B = gets.chomp
+
+    print "Enter the name of OF_1: "
+    OF_1 = gets.chomp
+
+    print "Enter the name of OF_2: "
+    OF_2 = gets.chomp
+
+    print "Enter the name of OF_3: "
+    OF_3 = gets.chomp
+
+    print "Enter the name of C: "
+    C = gets.chomp
+
+    print "Enter the name of P (enter number from list): "
+    pitcher_id = gets.chomp
+
+    create_lineup(1B, 2B, SS, 3B, OF_1, OF_2, OF_3, C, pitcher_id)
+
