688b08a8e9c2c7b0b74c932fb6ea542271cabfaf
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-28 21:16:22 -0500

    finished 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,9 +8,9 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+#get '/' do
+  #{}"#{params[:name]} is #{params[:age]} years old."
+#end
 
 # write a GET route with
 # route parameters
@@ -37,10 +37,38 @@ get '/students' do
   response
 end
 
+get '/contact' do
+  address = "215 n. Oketo, Chicago, Il"
+  address
+end
+
+get '/' do
+  name = params[:name]
+  if name
+    "Great job, #{name}!"
+  else
+    "Great job!"
+  end
+  end
+
+  get '/:num1/plus/:num2' do
+    num1 = params[:num1]
+    num2 = params[:num2]
+    result = (num1.to_i + num2.to_i)
+    result = result.to_s
+    "#{params[:num1]} plus #{params[:num2]} = #{result}"
+  end
 # write a GET route that retrieves
 # a particular student
 
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  student.to_s
+end
+
+get '/location/:campus' do
+  puts "Enter campus: (SD, SF, NYC, CHI, SEA)"
+  location = gets.chomp
+  student = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
   student.to_s
 end
