ea21cac4f6992fe53131ae3fadea942b32b209ba
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-04-29 00:14:36 -0500

    added event listener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,11 @@
-script.js
+console.log("script running!");
+
+
+
+function addBlackBorder() {
+  var col = document.getElementById("hello");
+  col.style.border = "10px solid black";
+}
+
+var col = document.getElementById("hello");
+col.addEventListener("click", addBlackBorder);
